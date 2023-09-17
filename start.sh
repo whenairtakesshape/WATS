530924bdb00b99ab5a8cd3a23bee0f581fa8b0c8
Ian <ianhome@ians-MBP-2.vc.shawcable.net>
2023-09-17 13:45:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ git pull
 cd "Server" || exit 1
 npm install
 npm install -g ts-node
-ts-node server.ts &> server.log &
+//ts-node server.ts &> server.log &
+ts-node server.ts  &
 
 cd "../Client" || exit 1
 npm install 
