de55a67490d8b26be80310fc246fed52a61d9e2e
Ian <iszufnara@gmail.com>
2024-01-28 10:49:37 -0800

    Update main.py

--- Pico/main.py
+++ Pico/main.py
@@ -22,11 +22,11 @@ POTENTIOMETER_3_PIN = 26
 def init():
     # Initialize Servos
     global servo_1 
-    servo_1 = PWM(Pin(SERVO_1_PIN))
+    servo_1 = PWM(Pin(SERVO_1_PIN, Pin.OUT), freq=50)
     global servo_2 
     servo_2 = PWM(Pin(SERVO_2_PIN))
     global servo_3
-    servo_3 = PWM(Pin(SERVO_3_PIN))
+    servo_3 = PWM(Pin(SERVO_3_PIN), freq=50)
     # Initialize Potentiometers
     global potentiometer_1
     potentiometer_1 = ADC(Pin(POTENTIOMETER_1_PIN))
@@ -50,6 +50,14 @@ def init():
     potentiometer_3_value = 0
 
 
+def set_servo_position(angle):
+    # Map the angle (0 to 180 degrees) to the pulse width (500 to 2500 microseconds)
+    pulse_width = int((angle / 270.0) * (2500 - 500) + 500)
+    
+    # Set the duty cycle based on the pulse width
+    servo_1.duty_u16(pulse_width * 65535 // 20000)  # Convert microseconds to duty cycle
+
+
 # Main 
 def main():
     #call init function
@@ -61,16 +69,18 @@ def main():
         potentiometer_2_value = potentiometer_2.read_u16()
         potentiometer_3_value = potentiometer_3.read_u16()
         #map potentiometer values to servo values
-        servo_1_value = map(potentiometer_1_value, [0, 65535], [0, 65535])
-        servo_2_value = map(potentiometer_2_value, [0, 65535], [0, 65535])
-        servo_3_value = map(potentiometer_3_value, [0, 65535], [0, 65535])
+        servo_1_value = int((potentiometer_1_value / 65535) * 10000)
+        servo_2_value = int((potentiometer_2_value / 65535) * 180)
+        servo_3_value = int((potentiometer_3_value / 65535) * 180)
         #print servo values
-        print(potentiometer_1_value)
+        print(servo_1_value)
         #write servo values
         servo_1.duty_u16(servo_1_value)
         servo_2.duty_u16(servo_2_value)
-        servo_3.duty_u16(servo_3_value)
+        #servo_3.duty_u16(servo_3_value)
+        #set_servo_position(servo_1_value)
         #sleep
         sleep(0.1)
         
 main()
+
