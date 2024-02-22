f14d87c3a1ef6603129e44531c6101dd18e2558d
Grant Li <ligrant1012@gmail.com>
2024-02-22 15:46:33 -0800

    remove older version of pollutant section code

--- Client/src/components/infoPageComponents/pollutantComponents/PollutantSection.tsx
+++ Client/src/components/infoPageComponents/pollutantComponents/PollutantSection.tsx
@@ -57,38 +57,6 @@ export const PollutantSection = (props: PollutantSectionProps) => {
           category={pollutantLogic.getSO2Category(props.so2)}
           indicator={pollutantLogic.getSO2Indicator(props.so2)} />
       </div>
-      {/** pollutant section top */}
-      {/* <div className="pollutant-section-container-top">
-        <PollutantCard name={"PM2.5"}
-          concentration={props.pm25}
-          category={pollutantLogic.getPM25Category(props.pm25)}
-          indicator={pollutantLogic.getPM25Indicator(props.pm25)} />
-        <PollutantCard name={"PM10"}
-          concentration={props.pm10}
-          category={pollutantLogic.getPM10Category(props.pm10)}
-          indicator={pollutantLogic.getPM10Indicator(props.pm10)} />
-        <PollutantCard name={"O3"}
-          concentration={props.o3}
-          category={pollutantLogic.getO3Category(props.o3)}
-          indicator={pollutantLogic.getO3Indicator(props.o3)} />
-      </div> */}
-
-      {/** pollutant section bottom */}
-      {/* <div className="pollutant-section-container-bottom">
-        <PollutantCard name={"CO"}
-          concentration={props.cO}
-          category={pollutantLogic.getCoCategory(props.cO)}
-          indicator={pollutantLogic.getCoIndicator(props.cO)} />
-        <PollutantCard name={"NO2"}
-          concentration={props.no2}
-          category={pollutantLogic.getNO2Category(props.no2)}
-          indicator={pollutantLogic.getNO2Indicator(props.no2)} />
-        <PollutantCard name={"SO2"}
-          concentration={props.so2}
-          category={pollutantLogic.getSO2Category(props.so2)}
-          indicator={pollutantLogic.getSO2Indicator(props.so2)} />
-      </div> */}
-
     </div>
   );
 };;
