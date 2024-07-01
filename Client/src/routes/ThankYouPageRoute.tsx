952e905d7351b9bb2f1af5e7251d7f1de7afb9c7
coffeemug17 <mitulpandey2@gmail.com>
2024-06-30 18:33:39 -0700

    style and structure changes

--- Client/src/routes/ThankYouPageRoute.tsx
+++ Client/src/routes/ThankYouPageRoute.tsx
@@ -11,24 +11,27 @@ export function ThankYouPage() {
     const navigate = useNavigate();
 
     return (
-        <div className="introduction-container-box">
-            <br />
-          <div className="">
-            <p>Thank you for your participation!</p>
-            <img src={attaLogo} alt='ATTA' />
-          </div>
-    
-          <div className="">
-            <div className="">
-              <p>
-                If you want to learn more about ATTA and stay tuned for future projects and events,
-                check out our website at attasociety.org or scan the QR code below!
-              </p>
+        <div className="thank-you-container">
+          <div className="thank-you-container-box">
+            <div className="thank-you-container-innerbox">
+                <div className="thank-you-container-01">
+                    <p>Thank you for your participation!</p>
+                    <img src={attaLogo} alt='ATTA' />
+                    <div className="thank-you-container-01-text">
+                        <p>
+                            If you want to learn more about ATTA and stay tuned for future projects and events,
+                            check out our website at attasociety.org or scan the QR code below!
+                        </p>
+                    </div>
+                </div>
+            
+                <div className="thank-you-container-02">
+                    <div className="">
+                        <img src={qrCode} alt='qrCode' />
+                    </div>   
+                </div>
             </div>
-            <div className="">
-                <img src={qrCode} alt='qrCode' />
-            </div>   
-            <div className="">
+            <div className="thank-you-container-03">
               <button onClick={() => navigate("/")}>Return to Home Page</button>
             </div>
           </div>
