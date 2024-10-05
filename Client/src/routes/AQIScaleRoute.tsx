d53b083890ffcb301094d52746f7ba51491a6643
arjunsingh3600 <swaniarjun@gmail.com>
2024-10-05 16:14:10 -0700

    AqiScale page formatting changes

--- Client/src/routes/AQIScaleRoute.tsx
+++ Client/src/routes/AQIScaleRoute.tsx
@@ -1,37 +1,10 @@
 // styles
 import "./css/aqiScaleRoute.scss";
 
-// assets
-
-// libraries
-import axios from "axios";
-
 // components
 import { AQIScale } from "../components/aqiScaleComponents/AQIScale";
 
 export const AQIScaleRoute = () => {
-
-  const postAQI = async (aqi : number) => {
-    try {
-      const res = await axios.post(`http://localhost:3001/aqi?value=${aqi}`);
-      console.log(res);
-      
-    } catch (error: any) {
-      console.error(error);
-      alert(error.message);
-    }
-  };
-
-  const postStopRequest = async() =>  {
-    try {
-      const res = await axios.post(`http://localhost:3001/command?command=s`);
-      console.log(res);
-
-    } catch (error: any) {
-      console.error(error);
-      alert(error.message);
-    }
-  };
 
   return (
     <div className="aqiscale-outer-container">  
@@ -54,7 +27,7 @@ export const AQIScaleRoute = () => {
           <p>
           It is important to be mindful that the AQI is a regionalised scale developed by local regulatory bodies. Each country and body can have widely varying AQI calculation methods reflecting
 
-          </p>
+          </p>  
 
           <p>
             ( A line about the AQI data we are using for the installation)
